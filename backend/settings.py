731ac03a9dd2c92350f7943f7f14671da1cee969
root <>
2022-02-20 15:38:48 +0300

    Community model

--- backend/settings.py
+++ backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'account',
+    'community', 
 
     'rest_framework',
     'rest_framework.authtoken',
---
+++ community/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class Community(models.Model):
+    title = models.CharField(verbose_name="Название Сообщества", max_length=32)
+    description = models.TextField(verbose_name="Описание Сообщества", max_length=3000)
+    owner = models.ForeignKey(
+            User,
+            related_name="Owner",
+            verbose_name="Глава сообщества", 
+            on_delete=models.CASCADE)
+    members = models.ManyToManyField(
+            User,
+            related_name="Members",
+            verbose_name="Участники группы"
+    )
+    
+
+
+    def __str__(self):
+        return self.title
+
